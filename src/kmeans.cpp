86bc8a82ab92b64983363ce3481398e93ef83e35
Sergey Bykov <captaintrunky@gmail.com>
2018-02-04 20:55:43 +0800

    fixing bug with kmeans++ initialization

--- src/kmeans.cpp
+++ src/kmeans.cpp
@@ -28,7 +28,7 @@ void KMeans::initialize(const Types::Points& ps, const size_t clusters_num) {
 void KMeans::_initialize(const Types::Points& ps, const size_t clusters_num) {
   mu().resize(clusters_num, ps.size2());
 
-  Types::Row(mu(), 0) = Geometry::pick_random_point(ps);
+  Types::Row(mu(), 0) = Geometry::pick_random_point(ps);s
 
   for (size_t c_idx = 1; c_idx < clusters_num; ++c_idx) {
     const auto weights = _compute_weights(c_idx, ps);
@@ -45,7 +45,7 @@ Types::Vector KMeans::_compute_weights(const size_t iter, const Types::Points& p
   for (size_t idx = 0; idx < iter; ++idx) {
     Types::Vector c_norm(ps_norms.size());
 
-    Types::RowConst c(mu(), iter);
+    Types::RowConst c(mu(), iter-1);
 
     std::fill(
         std::begin(c_norm),
@@ -83,7 +83,7 @@ Types::Vector KMeans::_compute_weights(const size_t iter, const Types::Points& p
       [=](const Types::CoordType v) { return v / total; }
   );
 
-  return Types::Column(d2, 0);
+  return res;
 }
 
 void KMeans::transform(const Types::Points& ps) const {
--- tests/include/test_kmeans.h
+++ tests/include/test_kmeans.h
@@ -8,20 +8,18 @@
 BOOST_AUTO_TEST_CASE( test_kmeans_initialization ) {
   KMeans kmeans;
 
-  Types::Points ps(10, 2);
+  Types::Points ps(8, 2);
   // cluster 1
   ps(0,0) = 0; ps(0,1) = 0;
   ps(1,0) = 1; ps(1,1) = 1;
   ps(2,0) = 0.5; ps(2,1) = 0.5;
   ps(3,0) = -0.5; ps(3,1) = -0.5;
-  ps(4,0) = 0; ps(4,1) = 0;
 
   // cluster 2
-  ps(5,0) = 10; ps(5,1) = 10;
-  ps(6,0) = 11; ps(6,1) = 11;
-  ps(7,0) = 10.5; ps(7,1) = 10.5;
-  ps(8,0) = -10.5; ps(8,1) = -10.5;
-  ps(9,0) = 10; ps(9,1) = 10;
+  ps(4,0) = 10; ps(4,1) = 10;
+  ps(5,0) = 11; ps(5,1) = 11;
+  ps(6,0) = 10.5; ps(6,1) = 10.5;
+  ps(7,0) = -10.5; ps(7,1) = -10.5;
 
   kmeans.initialize(ps, 2);
 
